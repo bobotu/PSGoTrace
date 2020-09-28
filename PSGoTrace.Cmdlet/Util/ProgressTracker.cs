4e1e27fd268f3b26348d5d866f72e153a8ff719c
Zejun Li <me@bobotu.dev>
2020-09-28 19:35:04 +0800

    stash

---
+++ PSGoTrace.Cmdlet/Util/ProgressTracker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Management.Automation;
+using PSGoTrace.Library;
+
+namespace PSGoTrace.Cmdlet.Util
+{
+    public class ProgressTracker : IProgressTracker
+    {
+        public IProgress StartTask(string name, string description, IProgress? parent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DoneTask(IProgress record)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Progress : IProgress
+    {
+        private readonly ProgressRecord _record;
+
+        public Progress(int id, string name, string description, int? parent = null)
+        {
+            _record = new ProgressRecord(id, name, description);
+            if (parent.HasValue) _record.ParentActivityId = parent.Value;
+        }
+
+        public int Id => _record.ActivityId;
+
+        public float PercentComplete
+        {
+            get => _record.PercentComplete;
+            set
+            {
+                _record.PercentComplete = (int) value;
+            }
+        }
+    }
+}
---
+++ PSGoTrace.Library/IProgress.cs
@@ -0,0 +1,14 @@
+﻿namespace PSGoTrace.Library
+{
+    public interface IProgress
+    {
+        public int Id { get; }
+        public float PercentComplete { get; set; }
+    }
+
+    public interface IProgressTracker
+    {
+        public IProgress StartTask(string name, string description, IProgress? parent);
+        public void DoneTask(IProgress record);
+    }
+}
