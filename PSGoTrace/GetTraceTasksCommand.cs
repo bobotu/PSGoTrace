7a6449b890b3263f5f36ce10042c2ecdb29fe0cf
Zejun Li <me@bobotu.dev>
2020-10-07 12:35:18 +0800

    new cmdlet

--- PSGoTrace/GetTraceTasksCommand.cs
+++ PSGoTrace/GetTraceTasksCommand.cs
@@ -1,21 +1,25 @@
 using System.Management.Automation;
+using PSGoTrace.Library;
 using PSGoTrace.Library.Types;
 
 namespace PSGoTrace
 {
     [Cmdlet(VerbsCommon.Get, "TraceTasks")]
-    [OutputType(typeof(int))]
+    [OutputType(typeof(UserTask))]
     public class GetTraceTasksCommand : PSCmdlet
     {
-        [Parameter(Mandatory = true,
-            Position = 0,
-            ValueFromPipeline = true,
-            ValueFromPipelineByPropertyName = true)]
+        [Parameter(Mandatory = true, Position = 0)]
         public TraceEvent[] TraceEvents { get; set; }
 
         protected override void ProcessRecord()
         {
-            WriteObject(TraceEvents.Length);
+            var goroutines = new GoroutinesStat(TraceEvents);
+            var tasks = new UserTasksStat(TraceEvents, goroutines);
+
+            foreach (var task in tasks.Values)
+            {
+                WriteObject(task);
+            }
         }
     }
 }
